479529d14718d5640e517ade0849f4d069a64a75
Ben Creasy <bcreasy@brightidea.com>
2017-08-31 21:15:30 -0700

    update anki version

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -2,9 +2,9 @@ cask 'anki-beta' do
     version '2.1.0beta4'
     sha256 'c36d1e621af4a3864e672d8bdb93764bb9c82bb889637ea4d7ae8df52ab23fc4'
   
-    url 'https://apps.ankiweb.net/downloads/beta/anki-2.1.0beta4.dmg'
+    url 'https://apps.ankiweb.net/downloads/beta/anki-2.1.0beta15.dmg'
     name 'Anki2-beta'
     homepage 'https://apps.ankiweb.net/'
   
-    app 'Anki.app'
+    app 'Anki-beta15.app'
   end
